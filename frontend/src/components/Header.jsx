ddbe827c9925c4b724a3cab292a9660f4008ee6a
liranUziel <liran.uzi.l@gmail.com>
2022-11-14 12:52:26 +0200

    Nav testing rePush

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -86,48 +86,49 @@ const Header = () => {
                 <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                 </button>
                 <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    <li id="" className="bg">
-                        <button className="text-white hover:text-green-400" onClick={GotoSafe}>
-                            <BsSafeFill/>
-                            My safe
-                        </button>
-                    </li>
-                    <li id="" className="header__list__item">
-                        <button className="text-white hover:text-green-400" onClick={GotoClass}>
-                            <FaChalkboardTeacher/>
-                            Class <span className="class_name">{className}</span>
-                        </button>
-                    </li>
-                    <li id="" className="text-white hover:text-green-400">
-                        <Link to='/home'></Link>
-                            <button className="text-white hover:text-green-400" onClick={GotoTournament}>
-                                <FaChessKing/>
-                                Tournament
+                    <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li id="" className="bg">
+                            <button className="text-white hover:text-green-400" onClick={GotoSafe}>
+                                <BsSafeFill/>
+                                My safe
+                            </button>
+                        </li>
+                        <li id="" className="header__list__item">
+                            <button className="text-white hover:text-green-400" onClick={GotoClass}>
+                                <FaChalkboardTeacher/>
+                                Class <span className="class_name">{className}</span>
                             </button>
                         </li>
                         <li id="" className="text-white hover:text-green-400">
-                            <button className="header__list__item_btn" onClick={GotoScoreBoard}>
-                                <BsFillBarChartFill/>
-                                Score Borad
-                                <span class="inline-flex justify-center items-center ml-2 w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
-                                    {score}
-                                </span>
-                            </button>
-                        </li>
-                        {user.userType === "student" ? (
-                        <>
-                        </>):(
-                            <li id="" className="text-white hover:text-green-400">
-                                <button className="header__list__item_btn" onClick={GotoDashboard}>
-                                    <RiAdminFill/>
-                                    Mangement
+                            <Link to='/home'></Link>
+                                <button className="text-white hover:text-green-400" onClick={GotoTournament}>
+                                    <FaChessKing/>
+                                    Tournament
                                 </button>
                             </li>
-                        )
-                        
-                        }
-                </ul>
+                            <li id="" className="text-white hover:text-green-400">
+                                <button className="header__list__item_btn" onClick={GotoScoreBoard}>
+                                    <BsFillBarChartFill/>
+                                    Score Borad
+                                    <span class="inline-flex justify-center items-center ml-2 w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
+                                        {score}
+                                    </span>
+                                </button>
+                            </li>
+                            {user.userType === "student" ? (
+                            <>
+                            </>):(
+                                <li id="" className="text-white hover:text-green-400">
+                                    <button className="header__list__item_btn" onClick={GotoDashboard}>
+                                        <RiAdminFill/>
+                                        Mangement
+                                    </button>
+                                </li>
+                            )
+                            
+                            }
+                    </ul>
+                </div>
                 <div className="user">
                     <Avatar name={realName} src={userImg} />
                     <span className="text-white">
@@ -139,7 +140,6 @@ const Header = () => {
                         Logout
                         <i className="fa-solid fa-right-from-bracket"></i>
                     </button>
-                </div>
                 </div>
             </div>
         </nav>
