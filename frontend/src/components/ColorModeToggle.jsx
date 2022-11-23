0d48265a217673d1d94a64b109daa78554e51b5f
GabrielMil <milshteingabriel@gmail.com>
2022-11-23 21:33:59 +0200

    small add in plugin

--- frontend/src/components/ColorModeToggle.jsx
+++ frontend/src/components/ColorModeToggle.jsx
@@ -34,7 +34,7 @@ const ColorModeToggle = () => {
 			type='button'
 			class='text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5'
 		>
-			{storedValue === 'light' ? 'Join the dark side' : 'Let there be Light'}
+			{storedValue === 'light' ? 'Join the dark side' : 'Let there be Light '}
 		</button>
 	);
 };
