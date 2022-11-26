7fb283d40a40ee702eb7824ee358a7bcc95997b6
liranUziel <liran.uzi.l@gmail.com>
2022-11-26 13:59:28 +0200

    Fix Logo, Fix Safe

--- frontend/src/pages/Home/components/ScoreBoard.jsx
+++ frontend/src/pages/Home/components/ScoreBoard.jsx
@@ -1,53 +1,63 @@
-import {  Modal,ModalOverlay,ModalContent,ModalHeader,ModalFooter,ModalBody,ModalCloseButton,} from '@chakra-ui/react'
-import { useDisclosure } from '@chakra-ui/react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
 
-import { Avatar } from '@chakra-ui/react'
-import { Button } from '@chakra-ui/react'
-import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import Safe from './utilsComponents/Safe';
+import { Avatar } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import Safe from "./utilsComponents/Safe";
 
 //get user name and use useState
 const HomeScoreBoard = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { user } = useSelector((state) => state.auth);
-  const [realName,setUsername] = useState("");
-  const [score,setScore] = useState(0);
-  const [userImg,setUserImg] = useState("");
-  useEffect(()=>{
+  const [realName, setUsername] = useState("");
+  const [score, setScore] = useState(0);
+  const [userImg, setUserImg] = useState("");
+  useEffect(() => {
     setUsername(user.realName);
     setScore(user.score);
-    setUserImg('');
-  },[user]);
+    setUserImg("");
+  }, [user]);
   const reader = new FileReader();
-  const thereIsChange = (e) =>{
+  const thereIsChange = (e) => {
     e.preventDefault();
-    const file  = e.target.files[0];
-    reader.readAsText(file,'UTF-8');
-    reader.onload = (e) =>{
-      const placeholder = document.getElementById('text');
+    const file = e.target.files[0];
+    reader.readAsText(file, "UTF-8");
+    reader.onload = (e) => {
+      const placeholder = document.getElementById("text");
       placeholder.innerHTML = e.target.result;
-    }
-  }
-
+    };
+  };
 
   return (
     <div className="flex flex-col items-center m-10 p-2 gap-2">
-      <Avatar name={realName} src={userImg} /> 
+      <Avatar name={realName} src={userImg} />
       <span>You Score is: {score}</span>
-      <Button colorScheme='teal' onClick={onOpen}>show leader board</Button>
+      <Button colorScheme="teal" onClick={onOpen}>
+        show leader board
+      </Button>
       {/* <input type="file" onChange={thereIsChange}/> */}
       {/* <div id="text"></div> */}
-      <Safe safe={{solved:false}} type={'private'}/>
-      <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false}  motionPreset='scale'>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        closeOnOverlayClick={false}
+        motionPreset="scale"
+      >
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Leader Boards</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            table of score
-            hellow
-          </ModalBody>
+          <ModalBody>table of score hellow</ModalBody>
 
           <ModalFooter>
             {/* <Button colorScheme='blue' mr={3} onClick={onClose}>
@@ -58,8 +68,7 @@ const HomeScoreBoard = () => {
         </ModalContent>
       </Modal>
     </div>
-    
-  )
-}
+  );
+};
 
-export default HomeScoreBoard
+export default HomeScoreBoard;
--- frontend/src/pages/Home/components/utilsComponents/Safe.jsx
+++ frontend/src/pages/Home/components/utilsComponents/Safe.jsx
@@ -17,7 +17,6 @@ import {
 } from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
-
 // import '../../styles/Safe.css'
 
 import { getSafe } from "../../../../utils/safes";
@@ -28,14 +27,14 @@ import SafeBreakPopUp from "./SafeBreakPopUp";
 const Safe = ({ safe: _safe, type }) => {
   //{safe:_safe,type,action}
   const { user } = useSelector((state) => state.auth);
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [safe, setSafe] = useState({});
   const [safeId, setSafeId] = useState("");
   const [name, setName] = useState("");
   const [popupActive, setPopupActive] = useState(false);
   const breakSafe = (e) => {
-    console.log(e.target.id);
-    setSafeId(e.target.id);
-    setPopupActive(true);
+    console.log(safeId);
+    // setSafeId(e.target.id);
   };
   const closeOverlay = (e) => {
     setPopupActive(false);
@@ -44,6 +43,7 @@ const Safe = ({ safe: _safe, type }) => {
   useEffect(() => {
     setSafe(_safe);
     setName(safe.safeName);
+    setSafeId(safe._id);
   }, [_safe]);
   const downloadSafe = async (e) => {
     // const response = await getSafe(user, safe._id);
@@ -69,33 +69,40 @@ const Safe = ({ safe: _safe, type }) => {
       id="safe"
     >
       <div className={`flex justify-end gap-2`} id="header">
-        {safe.solved ? (
-          ""
-        ) : (
-          <BsFillArrowDownCircleFill
-            onClick={downloadSafe}
-            className={`hover:text-green-600`}
-          />
-        )}
+        <BsFillArrowDownCircleFill
+          onClick={downloadSafe}
+          className={` ${
+            safe.solved
+              ? "text-gray-600"
+              : "hover:text-green-600 cursor-pointer"
+          }`}
+        />
         {type === "private" ? (
           <BsFillArrowUpCircleFill className={`hover:text-green-600`} />
         ) : (
           <></>
         )}
       </div>
-      <div className={`flex justify-center my-5 items-center`} id="main">
+      <div
+        className={`flex justify-center my-5 items-center relative group`}
+        id="main"
+      >
         {safe.solved ? (
-          <RiSafe2Fill className={`text-7xl z-0`} />
+          <RiSafe2Fill className={`text-7xl text-green-600`} />
         ) : (
-          <BsSafe className={`text-7xl z-0`} />
+          <BsSafe className={`text-7xl`} />
         )}
-        <button
-          className="z-10 text-xl font-semibold p-2 bg-green-500 rounded w-fit h-9"
-          id={safe._id}
-          onClick={breakSafe}
-        >
-          BREAK
-        </button>
+        {safe.solved ? (
+          <></>
+        ) : (
+          <button
+            className="z-10 text-xl font-semibold p-2 bg-green-500 rounded w-fit h-9 absolute hidden group-hover:block"
+            id={safe._id}
+            onClick={onOpen}
+          >
+            BREAK
+          </button>
+        )}
       </div>
       <div id="footer">
         <h3 className="">
@@ -103,6 +110,56 @@ const Safe = ({ safe: _safe, type }) => {
         </h3>
         <span className="">date: 15/8/2022 17:11 pm</span>
       </div>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        closeOnOverlayClick={false}
+        motionPreset="scale"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div class="flex items-center justify-center w-full">
+              <label
+                for="dropzone-file"
+                class="flex flex-col items-center justify-center w-full h-40 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              >
+                <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    aria-hidden="true"
+                    class="w-10 h-10 mb-3 text-gray-400"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                    ></path>
+                  </svg>
+                  <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span class="font-semibold">Click to upload</span> or drag
+                    and drop
+                  </p>
+                  <p class="text-xs text-gray-500 dark:text-gray-400">ASM</p>
+                </div>
+                <input id="dropzone-file" type="file" class="hidden" />
+              </label>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="teal" variant="outline" onClick={breakSafe}>
+              Submit
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
       {/*             
             <div className={safe.solved ? "safe__frame solved ":"safe__frame"}>
                 <div className="btn-array">
