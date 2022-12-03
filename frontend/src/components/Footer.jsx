4e9b327ce428f4096b7c3e3945222b8cf8840979
GabrielMil <milshteingabriel@gmail.com>
2022-12-03 21:06:09 +0200

    Header/Footer size and colormode icons

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,14 +1,14 @@
-import ColorModeToggle from "./ColorModeToggle";
+import ColorModeToggle from './ColorModeToggle';
 
 const Footer = () => {
-  return (
-    <footer className="flex dark:bg-dark-accenet-900 bg-light-accent-900 p-6 w-full justify-between">
-      <span className="block text-sm font-bold text-white">
-        © 2022 Liran Uziel && Gabriel Milshtein. All Rights Reserved.
-      </span>
-      <ColorModeToggle />
-    </footer>
-  );
+	return (
+		<footer className='flex dark:bg-dark-accenet-900 bg-light-accent-900 p-2 w-full justify-between items-center'>
+			<span className='block text-sm font-bold text-white'>
+				© 2022 Liran Uziel && Gabriel Milshtein. All Rights Reserved.
+			</span>
+			<ColorModeToggle />
+		</footer>
+	);
 };
 
 export default Footer;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,158 +1,146 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import { ReactComponent as DarkLogo } from "../assets/Images/dark_logo.svg";
-import { ReactComponent as LightLogo } from "../assets/Images/light_logo.svg";
+import { ReactComponent as DarkLogo } from '../assets/Images/dark_logo.svg';
+import { ReactComponent as LightLogo } from '../assets/Images/light_logo.svg';
 
-import { FaUser, FaChalkboardTeacher, FaChessKing } from "react-icons/fa";
-import { BsSafeFill, BsFillBarChartFill } from "react-icons/bs";
-import { Avatar } from "@chakra-ui/react";
-import { RiAdminFill } from "react-icons/ri";
+import { FaUser, FaChalkboardTeacher, FaChessKing } from 'react-icons/fa';
+import { BsSafeFill, BsFillBarChartFill } from 'react-icons/bs';
+import { Avatar } from '@chakra-ui/react';
+import { RiAdminFill } from 'react-icons/ri';
 
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link } from 'react-router-dom';
 
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from 'react-redux';
 
-import { logout, reset } from "../features/auth/authSlice";
-import { removeSafe, getSafe } from "../features/userSafe/userSafeSlice";
+import { logout, reset } from '../features/auth/authSlice';
+import { removeSafe, getSafe } from '../features/userSafe/userSafeSlice';
 
-import {
-  clearClass,
-  getClassInfo,
-  getClassSafes,
-} from "../features/class/classSlice";
+import { clearClass, getClassInfo, getClassSafes } from '../features/class/classSlice';
 
-import { Navbar, Dropdown } from "flowbite-react";
+import { Navbar, Dropdown } from 'flowbite-react';
 
 const Header = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.auth);
-  const { classInfo, isLoading, isError, isSuccess, message } = useSelector(
-    (state) => state.class
-  );
-  const [className, setClassName] = useState("");
-  const [realName, setUsername] = useState("");
-  const [userImg, setUserImg] = useState("");
-  const [score, setScore] = useState(0);
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const { user } = useSelector((state) => state.auth);
+	const { classInfo, isLoading, isError, isSuccess, message } = useSelector((state) => state.class);
+	const [className, setClassName] = useState('');
+	const [realName, setUsername] = useState('');
+	const [userImg, setUserImg] = useState('');
+	const [score, setScore] = useState(0);
 
-  useEffect(() => {
-    setUsername(user.realName);
-    setUserImg("");
-    setScore(user.score);
-  }, [user]);
+	useEffect(() => {
+		setUsername(user.realName);
+		setUserImg('');
+		setScore(user.score);
+	}, [user]);
 
-  useEffect(() => {
-    if (classInfo.length === 1) {
-      const { className, classNumber } = classInfo[0]["classInfo"];
-      setClassName(`${className} ${classNumber}`);
-    } else {
-      setClassName("");
-    }
-  }, [classInfo]);
+	useEffect(() => {
+		if (classInfo.length === 1) {
+			const { className, classNumber } = classInfo[0]['classInfo'];
+			setClassName(`${className} ${classNumber}`);
+		} else {
+			setClassName('');
+		}
+	}, [classInfo]);
 
-  useEffect(() => {
-    dispatch(getClassInfo(user));
-    // dispatch(getSafe(user));
-  }, []);
-  const onLogout = () => {
-    dispatch(logout());
-    dispatch(reset());
-    dispatch(clearClass());
-    dispatch(removeSafe());
-    navigate("/");
-  };
+	useEffect(() => {
+		dispatch(getClassInfo(user));
+		// dispatch(getSafe(user));
+	}, []);
+	const onLogout = () => {
+		dispatch(logout());
+		dispatch(reset());
+		dispatch(clearClass());
+		dispatch(removeSafe());
+		navigate('/');
+	};
 
-  const routes = [
-    {
-      path: "/home/safezone",
-      display: (
-        <>
-          <BsSafeFill /> My Safe
-        </>
-      ),
-    },
-    {
-      path: "/home/class",
-      display: (
-        <>
-          <FaChalkboardTeacher />
-          {className}
-        </>
-      ),
-    },
+	const routes = [
+		{
+			path: '/home/safezone',
+			display: (
+				<>
+					<BsSafeFill /> My Safe
+				</>
+			),
+		},
+		{
+			path: '/home/class',
+			display: (
+				<>
+					<FaChalkboardTeacher />
+					{className}
+				</>
+			),
+		},
 
-    {
-      path: "/home/tournament",
-      display: (
-        <>
-          <FaChessKing /> Tournament
-        </>
-      ),
-    },
-    {
-      path: "/home/scoreboard",
-      display: (
-        <>
-          <BsFillBarChartFill /> Score Board
-        </>
-      ),
-    },
-    {
-      path: "/home/dashboard",
-      display: (
-        <>
-          <RiAdminFill /> Managment
-        </>
-      ),
-      isAuthorized: true,
-    },
-  ];
+		{
+			path: '/home/tournament',
+			display: (
+				<>
+					<FaChessKing /> Tournament
+				</>
+			),
+		},
+		{
+			path: '/home/scoreboard',
+			display: (
+				<>
+					<BsFillBarChartFill /> Score Board
+				</>
+			),
+		},
+		{
+			path: '/home/dashboard',
+			display: (
+				<>
+					<RiAdminFill /> Managment
+				</>
+			),
+			isAuthorized: true,
+		},
+	];
 
-  return (
-    <header className="w-full dark:bg-dark-accenet-900 bg-light-accent-900 p-5">
-      <Navbar fluid={true} className="!bg-inherit">
-        {/* This is for the Logo */}
-        <Link to="/home">
-          <LightLogo className="h-12 sm:h-16 w-fit block dark:hidden" />
-          <DarkLogo className="h-12 sm:h-16 w-fit hidden dark:block" />
-        </Link>
+	return (
+		<header className='w-full dark:bg-dark-accenet-900 bg-light-accent-900 p-2'>
+			<Navbar fluid={true} className='!bg-inherit'>
+				{/* This is for the Logo */}
+				<Link to='/home'>
+					<LightLogo className='h-12 sm:h-13 w-fit block dark:hidden' />
+					<DarkLogo className='h-12 sm:h-13 w-fit hidden dark:block' />
+				</Link>
 
-        {/* This div is for the avatar */}
-        <div className="flex md:order-2 px-6">
-          <Dropdown
-            arrowIcon={false}
-            inline={true}
-            label={
-              <Avatar alt="User settings" name={user.realName} rounded="full" />
-            }
-          >
-            <Dropdown.Header>
-              <span className="block text-sm">{user.realName}</span>
-              <span className="block truncate text-sm font-medium">
-                {user.email}
-              </span>
-            </Dropdown.Header>
-            <Dropdown.Divider />
-            <Dropdown.Item onClick={onLogout}>Sign out</Dropdown.Item>
-          </Dropdown>
-          <Navbar.Toggle />
-        </div>
-        {/* Here I Create all the links */}
-        <Navbar.Collapse>
-          {routes.map((curr, index) => {
-            return curr?.isAuthorized === undefined ||
-              (curr.isAuthorized && user.userType !== "student") ? (
-              <Link to={curr.path} key={index}>
-                <span className="text-black dark:text-white">
-                  {curr.display}
-                </span>
-              </Link>
-            ) : null;
-          })}
-        </Navbar.Collapse>
-      </Navbar>
-    </header>
-  );
+				{/* This div is for the avatar */}
+				<div className='flex md:order-2 px-6'>
+					<Dropdown
+						arrowIcon={false}
+						inline={true}
+						label={<Avatar alt='User settings' name={user.realName} rounded='full' />}
+					>
+						<Dropdown.Header>
+							<span className='block text-sm'>{user.realName}</span>
+							<span className='block truncate text-sm font-medium'>{user.email}</span>
+						</Dropdown.Header>
+						<Dropdown.Divider />
+						<Dropdown.Item onClick={onLogout}>Sign out</Dropdown.Item>
+					</Dropdown>
+					<Navbar.Toggle />
+				</div>
+				{/* Here I Create all the links */}
+				<Navbar.Collapse>
+					{routes.map((curr, index) => {
+						return curr?.isAuthorized === undefined ||
+							(curr.isAuthorized && user.userType !== 'student') ? (
+							<Link to={curr.path} key={index}>
+								<span className='text-black dark:text-white'>{curr.display}</span>
+							</Link>
+						) : null;
+					})}
+				</Navbar.Collapse>
+			</Navbar>
+		</header>
+	);
 };
 
 export default Header;
