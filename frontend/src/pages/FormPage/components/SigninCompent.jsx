34cb270a240dbd661640a2a77a1d73ec3d6b81f5
liranUziel <liran.uzi.l@gmail.com>
2022-12-03 13:57:21 +0200

    Color of the bloody rainbow

--- frontend/src/pages/FormPage/components/SigninCompent.jsx
+++ frontend/src/pages/FormPage/components/SigninCompent.jsx
@@ -77,42 +77,11 @@ const LandingPageSignin = ({ isHidden }) => {
           onChange={onChange}
         ></Input>
         <FormHelperText>Please enter your Password</FormHelperText>
-        <Button className="mt-2" type="submit" colorScheme="accent_color">
+        <Button className="mt-2" type="submit" bg="dark-accenet.100">
           Let's Go!
         </Button>
       </FormControl>
     </form>
-    // <div id='signin-from'>
-    // 	<form action='#' className='signin-signup-form' onSubmit={onSubmit}>
-    // 		<label>user name</label>
-    // 		<input
-    // 			type='text'
-    // 			id='userName'
-    // 			name='userName'
-    // 			value={userName}
-    // 			required=''
-    // 			placeholder='please enter your user name'
-    // 			onChange={onChange}
-    // 		/>
-    // 		<label>Password</label>
-    // 		<input
-    // 			type='password'
-    // 			id='password'
-    // 			name='password'
-    // 			value={password}
-    // 			required=''
-    // 			placeholder='please enter your password'
-    // 			onChange={onChange}
-    // 		/>
-    // 		<button id='login-btn'>login</button>
-    // 		<button
-    // 			type='button'
-    // 			className='text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800'
-    // 		>
-    // 			Green
-    // 		</button>
-    // 	</form>
-    // </div>
   );
 };
 
--- frontend/src/pages/FormPage/components/SignupCompent.jsx
+++ frontend/src/pages/FormPage/components/SignupCompent.jsx
@@ -1,176 +1,147 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 //import { toast } from 'react-toastify';
-import Spinner from '../../../components/Spinner';
+import Spinner from "../../../components/Spinner";
 
 // Interact with storage
-import { useSelector, useDispatch } from 'react-redux';
-import { register, reset } from '../../../features/auth/authSlice';
+import { useSelector, useDispatch } from "react-redux";
+import { register, reset } from "../../../features/auth/authSlice";
+import { useColorModeValue } from "@chakra-ui/react";
 
-import { FormControl, FormLabel, FormHelperText, Input, Button, useToast } from '@chakra-ui/react';
+import {
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
 
 const LandingPageSingup = () => {
-	const toast = useToast();
+  const bg = useColorModeValue("light-accent.100", "dark-accenet.100");
+  const textColor = useColorModeValue("white", "light-accent.100");
+  const toast = useToast();
 
-	const [formData, setFormData] = useState({
-		realName: '',
-		userID: '',
-		email: '',
-		password: '',
-		confirmPassword: '',
-	});
-	const { realName, userID, userName, email, password, confirmPassword } = formData;
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    realName: "",
+    userID: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const { realName, userID, userName, email, password, confirmPassword } =
+    formData;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-	const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth);
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
 
-	const onChange = (e) => {
-		setFormData((prevState) => ({
-			...prevState,
-			[e.target.name]: e.target.value,
-		}));
-	};
-	useEffect(() => {
-		if (isError) {
-			toast({
-				title: message,
-				status: 'error',
-				isClosable: true,
-			});
-		}
-		if (isSuccess || user) {
-			navigate('/home');
-		}
-		dispatch(reset());
-	}, [user, isError, isSuccess, message, navigate, dispatch]);
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  useEffect(() => {
+    if (isError) {
+      toast({
+        title: message,
+        status: "error",
+        isClosable: true,
+      });
+    }
+    if (isSuccess || user) {
+      navigate("/home");
+    }
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
-	const onSubmit = (e) => {
-		// register
-		console.log('onSubmit');
-		e.preventDefault();
-		if (password !== confirmPassword) {
-			console.log('pass!==coPass');
-			toast({
-				title: 'Password does not match!',
-				status: 'error',
-				isClosable: true,
-			});
-		} else {
-			const userData = {
-				realName,
-				email,
-				password,
-				userType: 'student',
-				userId: userID,
-			};
-			//once we submit register reset the isError,isLoading,isSuccess
-			dispatch(register(userData));
-		}
-	};
-	if (isLoading) {
-		return <Spinner />;
-	}
-	return (
-		<>
-			<form onSubmit={onSubmit}>
-				<FormControl className='text-light-dark-color'>
-					<FormLabel>Full Name</FormLabel>
-					<Input type='text' name='realName' value={realName} onChange={onChange}></Input>
-					<FormHelperText>Please enter your Full Name</FormHelperText>
+  const onSubmit = (e) => {
+    // register
+    console.log("onSubmit");
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      console.log("pass!==coPass");
+      toast({
+        title: "Password does not match!",
+        status: "error",
+        isClosable: true,
+      });
+    } else {
+      const userData = {
+        realName,
+        email,
+        password,
+        userType: "student",
+        userId: userID,
+      };
+      //once we submit register reset the isError,isLoading,isSuccess
+      dispatch(register(userData));
+    }
+  };
+  if (isLoading) {
+    return <Spinner />;
+  }
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <FormControl className="text-light-dark-color">
+          <FormLabel>Full Name</FormLabel>
+          <Input
+            type="text"
+            name="realName"
+            value={realName}
+            onChange={onChange}
+          ></Input>
+          <FormHelperText>Please enter your Full Name</FormHelperText>
 
-					<FormLabel>User ID</FormLabel>
-					<Input type='text' name='userID' value={userID} onChange={onChange}></Input>
-					<FormHelperText>Please enter your user ID</FormHelperText>
+          <FormLabel>User ID</FormLabel>
+          <Input
+            type="text"
+            name="userID"
+            value={userID}
+            onChange={onChange}
+          ></Input>
+          <FormHelperText>Please enter your user ID</FormHelperText>
 
-					<FormLabel>Email</FormLabel>
-					<Input type='email' name='email' value={email} onChange={onChange}></Input>
-					<FormHelperText>Please enter your email</FormHelperText>
+          <FormLabel>Email</FormLabel>
+          <Input
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+          ></Input>
+          <FormHelperText>Please enter your email</FormHelperText>
 
-					<FormLabel>Password</FormLabel>
-					<Input type='password' name='password' value={password} onChange={onChange}></Input>
-					<FormHelperText>Please enter your Password</FormHelperText>
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          ></Input>
+          <FormHelperText>Please enter your Password</FormHelperText>
 
-					<FormLabel>Password Confirmation</FormLabel>
-					<Input type='password' name='confirmPassword' value={confirmPassword} onChange={onChange}></Input>
-					<FormHelperText>Please re-enter your Password</FormHelperText>
+          <FormLabel>Password Confirmation</FormLabel>
+          <Input
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={onChange}
+          ></Input>
+          <FormHelperText>Please re-enter your Password</FormHelperText>
 
-					<br />
-					<Button type='submit' colorScheme='accent_color'>
-						Sign Me Up
-					</Button>
-				</FormControl>
-			</form>
-		</>
-		// <div id='signup-from'>
-		// 	<form action='#' className='signin-signup-form' id='signup-form' onSubmit={onSubmit}>
-		// 		<label>full Name</label>
-		// 		<input
-		// 			type='text'
-		// 			id='realName'
-		// 			name='realName'
-		// 			value={realName}
-		// 			required=''
-		// 			placeholder='please enter your full name'
-		// 			onChange={onChange}
-		// 		/>
-		// 		<label>user ID</label>
-		// 		<input
-		// 			type='text'
-		// 			id='userID'
-		// 			name='userID'
-		// 			value={userID}
-		// 			required=''
-		// 			pattern='[0-9]{9}'
-		// 			placeholder='please enter your user ID'
-		// 			onChange={onChange}
-		// 		/>
-		// 		<label>user name</label>
-		// 		<input
-		// 			type='text'
-		// 			id='userName'
-		// 			name='userName'
-		// 			value={userName}
-		// 			required=''
-		// 			placeholder='please enter your user name'
-		// 			onChange={onChange}
-		// 			disabled
-		// 		/>
-		// 		<label>email</label>
-		// 		<input
-		// 			type='email'
-		// 			id='email'
-		// 			name='email'
-		// 			value={email}
-		// 			required=''
-		// 			placeholder='please enter your email'
-		// 			onChange={onChange}
-		// 		/>
-		// 		<label>password</label>
-		// 		<input
-		// 			type='password'
-		// 			id='password'
-		// 			name='password'
-		// 			value={password}
-		// 			required=''
-		// 			placeholder='please enter your password'
-		// 			onChange={onChange}
-		// 		/>
-		// 		<label>password confiremation</label>
-		// 		<input
-		// 			type='password'
-		// 			id='confirmPassword'
-		// 			name='confirmPassword'
-		// 			value={confirmPassword}
-		// 			required=''
-		// 			placeholder='please re-enter your passord to confirem'
-		// 			onChange={onChange}
-		// 		/>
-		// 		<button id='login-btn'>signup</button>
-		// 	</form>
-		// </div>
-	);
+          <br />
+          <Button className="mt-2" type="submit" bg={bg} color={textColor}>
+            Sign Me Up
+          </Button>
+        </FormControl>
+      </form>
+    </>
+  );
 };
 
 export default LandingPageSingup;
